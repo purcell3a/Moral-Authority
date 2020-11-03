ca2106b63717276e0a472268769c73649d53022a
Angela Purcell <kalmpurcell@gmail.com>
2020-11-03 22:42:13 +0000

    made general model.py classes

--- model.py
+++ model.py
@@ -1,40 +1,174 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
-# class Card(db.Model):
-#     """Card in trading cards website."""
+*****************************************************************************
 
-#     __tablename__ = "cards"
+class User(db.Model):
+    """A user."""
 
-#     card_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     name = db.Column(db.String(64), nullable=False)
-#     skill = db.Column(db.String(64), nullable=True)
-#     image_url = db.Column(db.String(250), nullable=True)
-    
+    __tablename__ = 'users'
 
-#     def __repr__(self):
-#         """Provide helpful representation when printed."""
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    fname = db.Column(db.String,
+                        nullable=False, 
+                        unique=False)
+    lname = db.Column(db.String,
+                        nullable=False, 
+                        unique=False)
+    email = db.Column(db.String,
+                        nullable=False, 
+                        unique=True)
+    password = db.Column(db.String,
+                        nullable=False)
+    favorite = db.Column(db.Integer,
+                        nullable=True)
+    product_add = db.Column(db.Integer,
+                        nullable=True)
 
-#         return f"<Card card_id={self.card_id} name={self.name}>"
+    # rating = a list of Rating objects???
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+class Favorite(db.Model):
+    """A users favorite product."""
+
+    __tablename__ = 'favorites'
+
+    favorite_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key= True)
+    category = db.Column(db.Integer, 
+                        db.ForeignKey('users.user_id'))
+    product = db.Column(db.Integer, 
+                        db.ForeignKey('users.user_id'))
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+
+    def __repr__(self):
+        return f'<Rating rating_id={self.rating_id} movie_id={self.movie_id} rating={self.rating}>'
+
+class Category(db.Model):
+    """A category."""
+
+    __tablename__ = 'categories'
+
+    category_id = db.Column(db.Integer,
+                        nullable=False)
+    title = db.Column(db.String,
+                        autoincrement=True,
+                        primary_key=True)
+    subcategory = db.Column(db.Integer,
+                        nullable=False)
+
+    def __repr__(self):
+        return f'<movie_id={self.movie_id} title={self.title} description={self.description}>'
+
+class Subcategory(db.Model):
+    """A category."""
+
+    __tablename__ = 'subcategories'
+
+    subcategory_id = db.Column(db.Integer,
+                        nullable=False)
+    title = db.Column(db.String,
+                        autoincrement=True,
+                        primary_key=True)
+
+    def __repr__(self):
+        return f'<movie_id={self.movie_id} title={self.title} description={self.description}>'
 
 
-# def connect_to_db(app):
-#     """Connect the database to our Flask app."""
+class Certification(db.Model):
+    """A category."""
 
-#     # Configure to use our PostgreSQL database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cards'
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     db.app = app
-#     db.init_app(app)
+    __tablename__ = 'certifications'
+
+    certification_id = db.Column(db.Integer,
+                        nullable=False)
+    company = db.Column(db.String,
+                        autoincrement=True,
+                        primary_key=True)
 
 
-# if __name__ == "__main__":
-#     # As a convenience, if we run this module interactively, it will leave
-#     # you in a state of being able to work with the database directly.
+    def __repr__(self):
+        return f'<movie_id={self.movie_id} title={self.title} description={self.description}>'
 
-#     from server import app
-#     connect_to_db(app)
-#     print("Connected to DB.")
-def test(this):
-    print('why u no show up')
+
+
+class Product(db.Model):
+    """A category."""
+
+    __tablename__ = 'products'
+
+    product_id = db.Column(db.Integer,
+                        nullable=False,
+                        primary_key=True)
+    title = db.Column(db.String,
+                        autoincrement=True)
+    url = db.Column(db.String,
+                        autoincrement=True)
+    description = db.Column(db.String,
+                        autoincrement=True)
+    category = db.Column(db.Integer,
+                        nullable=False)
+    subcategory = db.Column(db.Integer,
+                        nullable=False)
+
+    def __repr__(self):
+        return f'<movie_id={self.movie_id} title={self.title} description={self.description}>'
+
+class Product_Add(db.Model):
+    """A category."""
+
+    __tablename__ = 'productsadded'
+
+    product_id = db.Column(db.Integer,
+                        nullable=False,
+                        primary_key=True)
+    user_id = db.Column(db.Integer,
+                        nullable=False,
+                        db.ForeignKey=True)
+
+    title = db.Column(db.String,
+                        autoincrement=True)
+    url = db.Column(db.String,
+                        autoincrement=True)
+    description = db.Column(db.String,
+                        autoincrement=True)
+    category = db.Column(db.Integer,
+                        nullable=False)
+    subcategory = db.Column(db.Integer,
+                        nullable=False)
+
+    def __repr__(self):
+        return f'<movie_id={self.movie_id} title={self.title} description={self.description}>'
+
+def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    connect_to_db(app)
+    # print("Connected to DB.")
