10c1ee590b59df0eea047667f844b19dea8d3b5e
Angela Purcell <kalmpurcell@gmail.com>
2020-11-05 01:00:49 +0000

    react login page rendering

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,60 +1,13 @@
-
-// REACT TRADING CARDS 2
-
-// *******************************************************************
-// function Homepage() {
-//     return (
-//       <React.Fragment>
-//         <img src="/static/img/balloonicorn.jpg" />
-  
-//         <p>This is a great site for viewing trading cards.</p>
-  
-//         <a href="/cards-jquery">
-//           Click here to view the trading cards page in JQuery.
-//         </a>
-//         <br/>
-//         <a href="/cards">
-//           Click here to view the trading cards page in React.
-//         </a>
-//       </React.Fragment>
-//     );
-//   }
-  
-//   ReactDOM.render(<Homepage />, document.getElementById('app'));
-//******************************************************************* */
-
-// REACT TRADING CARDS 1
-
-// *******************************************************************
 // "use strict";
 
-// function Homepage() {
-//   return (
-//     <React.Fragment>
-//       <p>Welcome User! Wow you're so shiny!!!</p>
-//       <a href="/cards">Click here to lay your eyeballs on some cards</a>
-//       <img src="/static/img/balloonicorn.jpg"></img>
-//     </React.Fragment>>
-//   );
-// }
-
-// ReactDOM.render(<Homepage />, document.querySelector('#app'));
-
-// ReactDOM.render(element, container[, callback])
-//******************************************************************* */
-
-
-
-<!-- 
-function Homepage(props) {
-  return (
-    <React.Fragment>
-      <h1>Welcome to Balloonicorn's Trading Card Site!</h1>
-      <a href="/cards">Go to the cards page</a>
-      <img href="/static/img/balloonicorn.jpg" />
-    </React.Fragment>
-  );
-}
-
-ReactDOM.render(<Homepage />, document.getElementById('app')); -->
-sorr
+// *******************************************************************
+function Homepage() {
+    return (
+      <React.Fragment>
+            <p> this is my homepage</p>
+      </React.Fragment>
+    );
+  }
+  
+  ReactDOM.render(<Homepage />, document.getElementById('app'));
+// ******************************************************************* */
---
+++ static/js/product.jsx
@@ -0,0 +1,11 @@
+// *******************************************************************
+function Product() {
+    return (
+      <React.Fragment>
+            <p> this is my product page</p>
+      </React.Fragment>
+    );
+  }
+  
+  ReactDOM.render(<Product />, document.getElementById('app'));
+// ******************************************************************* */
