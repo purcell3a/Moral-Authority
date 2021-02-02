a127d8f804d7e0ff70f148d5ce0a58a7f9d4eaa9
Angela Purcell <kalmpurcell@gmail.com>
2021-02-02 21:20:53 +0000

    rm from flask login

--- model.py
+++ model.py
@@ -1,6 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from datetime import datetime
 
 
